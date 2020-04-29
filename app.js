7d92788ae5413f6cab16e45436fdd99f297bb7ae
premkestrel <pushpapremradan@gmail.com>
2020-04-29 23:02:32 +0530

    some changes in mean app deploy on heroku

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname,'dist')));
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public/dist/index.html'));
+    res.sendFile(path.join(__dirname,'dist/index.html'));
 })
 //routing
 app.use("/api",route);
